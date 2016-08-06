81d5bb195d146a2bdcf7c04003c40dd2d82ad7a7
newfour <realnewfour@gmail.com>
2016-08-07 00:45:06 +0800

    start reading collapse

--- bootstrap_reading/js/05_collapse.js
+++ bootstrap_reading/js/05_collapse.js
@@ -6,43 +6,59 @@
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
-
+// 折叠
 +function ($) {
   'use strict';
 
   // COLLAPSE PUBLIC CLASS DEFINITION
   // ================================
+  // collapse 插件类定义
 
   var Collapse = function (element, options) {
+    // 折叠元素
     this.$element      = $(element)
     this.options       = $.extend({}, Collapse.DEFAULTS, options)
+    // 触发元素，用于触发当前折叠元素发生折叠动画
+    // 触发元素需要有属性 data-toggle="collapse" 且通过 href/data-target 指定了它所对应的折叠元素的 id
     this.$trigger      = $('[data-toggle="collapse"][href="#' + element.id + '"],' +
                            '[data-toggle="collapse"][data-target="#' + element.id + '"]')
     this.transitioning = null
 
+    // 参数中指定了父
     if (this.options.parent) {
+      // 调用原型方法 getParent
       this.$parent = this.getParent()
     } else {
       this.addAriaAndCollapsedClass(this.$element, this.$trigger)
     }
 
+    //
     if (this.options.toggle) this.toggle()
   }
 
+  // 版本号
   Collapse.VERSION  = '3.3.5'
 
+  // 过渡动画的时间
   Collapse.TRANSITION_DURATION = 350
 
+  // 默认配置
   Collapse.DEFAULTS = {
-    toggle: true
+    toggle: true  // 默认切换
   }
 
+  // 原型方法 dimension
   Collapse.prototype.dimension = function () {
+    // 判断折叠元素上有无 width 类样式
     var hasWidth = this.$element.hasClass('width')
     return hasWidth ? 'width' : 'height'
   }
 
+  // 原型方法 show
   Collapse.prototype.show = function () {
+    // 正在过渡或者元素上面有 .in 类样式了，直接返回
+    // 折叠元素有 .in 类样式表明它已经展开了（已经 show 了）
+    // .collapse.in { display:block }
     if (this.transitioning || this.$element.hasClass('in')) return
 
     var activesData
@@ -53,8 +69,11 @@
       if (activesData && activesData.transitioning) return
     }
 
+    // 定义一个事件对象 startEvent
     var startEvent = $.Event('show.bs.collapse')
+    // 触发 show 事件
     this.$element.trigger(startEvent)
+    // 可以阻止 show 事件来阻止 show 动画执行
     if (startEvent.isDefaultPrevented()) return
 
     if (actives && actives.length) {
@@ -64,6 +83,7 @@
 
     var dimension = this.dimension()
 
+    //
     this.$element
       .removeClass('collapse')
       .addClass('collapsing')[dimension](0)
@@ -131,10 +151,15 @@
       .emulateTransitionEnd(Collapse.TRANSITION_DURATION)
   }
 
+  // 切换折叠元素的 show/hide 状态
   Collapse.prototype.toggle = function () {
+    // .in 代表折叠元素是显示的
+    // 显示的隐藏之，隐藏的显示之
     this[this.$element.hasClass('in') ? 'hide' : 'show']()
   }
 
+  // 原型方法 getParent
+  // 配置里面指定了 parent
   Collapse.prototype.getParent = function () {
     return $(this.options.parent)
       .find('[data-toggle="collapse"][data-parent="' + this.options.parent + '"]')
@@ -145,29 +170,39 @@
       .end()
   }
 
+  // 原型方法 addAriaAndCollapsedClass
   Collapse.prototype.addAriaAndCollapsedClass = function ($element, $trigger) {
+    // 折叠元素是否展开的标识
     var isOpen = $element.hasClass('in')
-
+    // 告诉辅助工具当前折叠元素的展开状态
     $element.attr('aria-expanded', isOpen)
+    // 触发元素
     $trigger
+      // 触发元素上的样式相应作出改变（折叠元素 展开/闭合 了则相应 无/有 类样式 collapse）
       .toggleClass('collapsed', !isOpen)
+      // 告诉辅助工具触发元素上触发了对应折叠元素的 展开/闭合 状态
       .attr('aria-expanded', isOpen)
   }
 
+  // 原型方法 getTargetFromTrigger
+  // 根据触发元素上的属性 data-target 或者 href 来确定其对应的折叠元素
   function getTargetFromTrigger($trigger) {
     var href
     var target = $trigger.attr('data-target')
       || (href = $trigger.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') // strip for ie7
 
+    // 返回目标折叠元素
     return $(target)
   }
 
 
   // COLLAPSE PLUGIN DEFINITION
   // ==========================
+  // collapse 插件定义，即 collapse 的 js 函数用法
 
   function Plugin(option) {
     return this.each(function () {
+      // 每个折叠元素
       var $this   = $(this)
       var data    = $this.data('bs.collapse')
       var options = $.extend({}, Collapse.DEFAULTS, $this.data(), typeof option == 'object' && option)
@@ -178,14 +213,18 @@
     })
   }
 
+  // 保存 $.fn 中可能已经存在了的 collapse 插件方法
   var old = $.fn.collapse
 
+  // 将插件注册到 $.fn 中去
   $.fn.collapse             = Plugin
+  // 通过 Constructor 属性暴露了其原始的构造函数
   $.fn.collapse.Constructor = Collapse
 
 
   // COLLAPSE NO CONFLICT
   // ====================
+  // 防冲突处理
 
   $.fn.collapse.noConflict = function () {
     $.fn.collapse = old
@@ -195,6 +234,7 @@
 
   // COLLAPSE DATA-API
   // =================
+  // data-api 用法（纯 HTML）
 
   $(document).on('click.bs.collapse.data-api', '[data-toggle="collapse"]', function (e) {
     var $this   = $(this)
